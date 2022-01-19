7d2d9cd7f0e7d29069800f5dd08ce7d0ad37cd07
alexy247 <alyona.ogloblina@mail.ru>
2022-01-20 01:15:58 +0300

    Tasks 2 and 3

--- Results/check_permissions.sql
+++ Results/check_permissions.sql
@@ -1,6 +1,22 @@
-CREATE PROCEDURE CheckPermissions
-AS
+-- Проверка и вывод прав 
+
+-- Задание. Написать хранимую процедуру, выдающую все права пользователя на действия в каждой таблице текущей БД
+
+CREATE PROCEDURE CheckPermissions AS
 BEGIN
-    SELECT entity_name, permission_name 
-    FROM fn_my_permissions((SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES), 'OBJECT')
+	SELECT * INTO #AllPermissionsTable FROM fn_my_permissions(NULL, 'SERVER') UNION SELECT * FROM fn_my_permissions(NULL, 'DATABASE');
+	DECLARE p_cur CURSOR FOR
+		SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+        OPEN p_cur
+        DECLARE @t_name SYSNAME
+        FETCH NEXT FROM p_cur INTO @t_name
+        WHILE @@FETCH_STATUS = 0
+        BEGIN
+            INSERT INTO #AllPermissionsTable SELECT * FROM fn_my_permissions(@t_name, 'Object')
+            FETCH NEXT FROM p_cur INTO @t_name
+        END
+        CLOSE p_cur
+	DEALLOCATE p_cur
+	SELECT * FROM #AllPermissionsTable
+	DROP TABLE #AllPermissionsTable
 END
